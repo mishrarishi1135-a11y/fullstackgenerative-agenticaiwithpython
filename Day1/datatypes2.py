58e4e2948f3974a2fc2aa114b12a2c25d79f0a43
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-08-21 16:36:07 +0530

    integer data types

--- Day1/datatypes2.py
+++ Day1/datatypes2.py
@@ -8,3 +8,29 @@ print(f"total grams of base tea is {total_grams}")
 
 remaining_tea = black_tea_grams - ginger_grams
 print(f"Total grams of remaining tea is {remaining_tea}")
+
+milk_liters = 7
+servings = 4
+milk_per_serving = milk_liters / servings
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_bag = 7
+pots = 4
+bags_per_pot = total_tea_bag // pots
+print(f"while Tea bags per pot: {bags_per_pot}")
+
+total_cardamom_pods = 10
+pods_per_cup = 3
+leftover_pods = total_cardamom_pods % pods_per_cup
+print(f"leftover cardamom pods: {leftover_pods}")
+
+base_flavour_strength = 2
+scale_factor = 3
+powerful_flavour = base_flavour_strength ** scale_factor
+print(f"Scaled flavour strength: {powerful_flavour}")
+
+total_tea_leaves_harvested = 1_000_000_000
+print(f"tea leaves: {total_tea_leaves_harvested}")
+
+#this is all about the integer..
+
