72be88badba6fa1a6f69567d8aea1ff1c223c89a
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-08-21 18:24:14 +0530

    tuples and membership testing'

---
+++ Day1/datatypes6.py
@@ -0,0 +1,13 @@
+masala_spices = ("cardamom", "cloves", "cinnamon")
+
+(spice1, spice2, spice3) = masala_spices
+print(f"Main masala spices: {spice1}, {spice2}, {spice3}")
+
+ginger_ratio, cardamom_ratio = 2, 1
+print(f"Ratio is G: {ginger_ratio} and C: {cardamom_ratio}")
+ginger_ratio, cardamom_ratio = cardamom_ratio, ginger_ratio
+print(f"Ratio is G: {ginger_ratio} and C: {cardamom_ratio}")
+
+#membership testing
+
+print(f"Is ginger in masala_spices ? {'ginger' in masala_spices}")
