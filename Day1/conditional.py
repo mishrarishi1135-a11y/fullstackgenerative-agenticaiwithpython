c4ad07d6c0680dab57e1ddca862c696492a7d1f4
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-08-21 23:30:54 +0530

    kettle notification system

---
+++ Day1/conditional.py
@@ -0,0 +1,7 @@
+# you are creating a notification system for a smart kettle.
+# it should remind the user only when the kettle has finished boiling.
+
+Kettle_boiled = True
+
+if Kettle_boiled:
+    print("Kettle Done! time to make chai")
