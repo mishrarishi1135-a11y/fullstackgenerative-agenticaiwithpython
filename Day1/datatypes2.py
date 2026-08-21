431c65e4ee70ccacdc233a0d97df39d5cb0e89ff
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-08-21 16:22:07 +0530

    integer data type

---
+++ Day1/datatypes2.py
@@ -0,0 +1,10 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_grams = black_tea_grams + ginger_grams
+print(f"total grams of base tea is {total_grams}")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"Total grams of remaining tea is {remaining_tea}")
