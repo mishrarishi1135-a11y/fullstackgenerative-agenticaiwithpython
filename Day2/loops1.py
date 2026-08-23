043537a307f10275920e64c1c2962f77512838a8
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-08-23 08:56:10 +0530

    tea dispenser system

---
+++ Day2/loops1.py
@@ -0,0 +1,7 @@
+# a tea stall owner has a digital token display.
+# for every customer in a line, a token number is printed and chai is served.
+# use for loop to generate 1 to 10 numbers for tokens.
+# print: "serving chai to token[number]".
+
+for token in range(1,11):
+    print(f"Serving chai to token #{token}")
