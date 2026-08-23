644e07cd41a753fdc5041701f97f6f6ada1bc41e
anujmishra1823-boop <anujmishra1823@gmail.com>
2026-08-23 09:03:39 +0530

    batch chai preparation

---
+++ Day2/loops2.py
@@ -0,0 +1,6 @@
+# A chai shop makes tea in batches every 15 minutes,
+# you want to simulate batches.
+# Task: use range() to simulate batch numbers
+
+for batch in range(1, 5):
+    print(f"preparing chai for batch #{batch}")
